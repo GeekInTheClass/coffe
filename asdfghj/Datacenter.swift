0aec7a1b800539df4f159ace427c3d7dc16a1e44
BONG <bong@BONGui-MacBookPro.local>
2019-04-18 17:05:08 +0900

    first

---
+++ asdfghj/Datacenter.swift
@@ -0,0 +1,29 @@
+//
+//  Datacenter.swift
+//  asdfghj
+//
+//  Created by BONG on 18/04/2019.
+//  Copyright © 2019 BONG. All rights reserved.
+//
+
+import Foundation
+
+struct Coffe {
+    var cost : Int = 0
+    var name : String = ""
+    
+}
+
+var coffeSubscript : [Coffe] = []
+
+var co1 = Coffe(cost: 1500, name: "아이스아메리카노")
+var co2 = Coffe(cost: 1700, name: "아이스카페라떼")
+var co3 = Coffe(cost: 2000, name: "따뜻한카페라떼")
+var co4 = Coffe(cost: 2100, name: "녹차라떼")
+var co5 = Coffe(cost: 3000, name: "카라멜마끼야또")
+var co6 = Coffe(cost: 1800, name: "따뜻한아메리카노")
+var co7 = Coffe(cost: 1600, name: "카푸치노")
+var co8 = Coffe(cost: 2200, name: "사과주스")
+var co9 = Coffe(cost: 3000, name: "오렌지주스")
+var co10 = Coffe(cost: 2500, name: "수박주스")
+
---
+++ asdfghj/asdfghj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  asdfghj
+//
+//  Created by BONG on 18/04/2019.
+//  Copyright © 2019 BONG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
