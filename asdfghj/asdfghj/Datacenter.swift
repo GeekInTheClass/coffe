b8588a62ed300b431e1665b882501914fa24c462
BONG <bong@BONGui-MacBookPro.local>
2019-04-18 18:10:21 +0900

    3th

--- asdfghj/asdfghj/Datacenter.swift
+++ asdfghj/asdfghj/Datacenter.swift
@@ -8,22 +8,22 @@
 
 import Foundation
 
-struct Coffe {
+struct Coffee {
     var cost : Int = 0
     var name : String = ""
     
 }
 
-var coffeSubscript : [Coffe] = []
+var coffeSubscript : [Coffee] = []
 
-var co1 = Coffe(cost: 1500, name: "아이스아메리카노")
-var co2 = Coffe(cost: 1700, name: "아이스카페라떼")
-var co3 = Coffe(cost: 2000, name: "따뜻한카페라떼")
-var co4 = Coffe(cost: 2100, name: "녹차라떼")
-var co5 = Coffe(cost: 3000, name: "카라멜마끼야또")
-var co6 = Coffe(cost: 1800, name: "따뜻한아메리카노")
-var co7 = Coffe(cost: 1600, name: "카푸치노")
-var co8 = Coffe(cost: 2200, name: "사과주스")
-var co9 = Coffe(cost: 3000, name: "오렌지주스")
-var co10 = Coffe(cost: 2500, name: "수박주스")
+var co1 = Coffee(cost: 1500, name: "아이스아메리카노")
+var co2 = Coffee(cost: 1700, name: "아이스카페라떼")
+var co3 = Coffee(cost: 2000, name: "따뜻한카페라떼")
+var co4 = Coffee(cost: 2100, name: "녹차라떼")
+var co5 = Coffee(cost: 3000, name: "카라멜마끼야또")
+var co6 = Coffee(cost: 1800, name: "따뜻한아메리카노")
+var co7 = Coffee(cost: 1600, name: "카푸치노")
+var co8 = Coffee(cost: 2200, name: "사과주스")
+var co9 = Coffee(cost: 3000, name: "오렌지주스")
+var co10 = Coffee(cost: 2500, name: "수박주스")
 
---
+++ asdfghj/asdfghj/ccTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ccTableViewController.swift
+//  asdfghj
+//
+//  Created by BONG on 18/04/2019.
+//  Copyright © 2019 BONG. All rights reserved.
+//
+
+import UIKit
+
+class ccTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        coffeSubscript.append(co1)
+        coffeSubscript.append(co2)
+        coffeSubscript.append(co3)
+        coffeSubscript.append(co4)
+        coffeSubscript.append(co5)
+        coffeSubscript.append(co6)
+        coffeSubscript.append(co7)
+        coffeSubscript.append(co8)
+        coffeSubscript.append(co9)
+        coffeSubscript.append(co10)
+        
+
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return coffeSubscript.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Coffee", for: indexPath)
+
+        // Configure the cell...
+//        if indexPath.row > 4{
+//            cell.textLabel?.text = "a"
+//            cell.detailTextLabel?.text = "b"
+//        } else {
+//            cell.textLabel?.text = "c"
+//            cell.detailTextLabel?.text = "d"
+//        }
+        let kk = coffeSubscript[indexPath.row]
+        cell.textLabel?.text = String(kk.cost)
+        cell.detailTextLabel?.text = kk.name
+        
+        
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
